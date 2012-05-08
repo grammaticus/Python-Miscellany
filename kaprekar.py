f53de9d46b87fd9babda4aa931116d6da965b4d0
John H Moore <grammaticus@grammaticus.us>
2012-05-08 17:59:44 -0600

    fixed bug in kaprekar.py

--- kaprekar.py
+++ kaprekar.py
@@ -1,14 +1,16 @@
 # Kaprekar Number - consider an n-digit number k; square it and add the right n
 # digits to the left n or n-1 digits; if it equals k, then k is a Kaprekar Number
 def is_kaprekar(k):
-	if k == 1:
-		return True
-	elif k > 8:
+	if k < 10:
+		if k == 1 or k == 9:
+			return True
+		else:
+			return False
+	else:
 		n = len(str(k))
-		k_sqr = k**2
-		k_sqr_str = str(k_sqr)
-		k_sqr_left = k_sqr_str[n:]
-		k_sqr_right = k_sqr_str[:n]
+		k_sqr = str(k**2)
+		k_sqr_left = k_sqr[n-n%2:]
+		k_sqr_right = k_sqr[:n-n%2]
 		k_summed = int(k_sqr_left) + int(k_sqr_right)
 		return k == k_summed
 	return False
